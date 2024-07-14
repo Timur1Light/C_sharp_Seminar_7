e524bbaaa61e6f5ef0ac2f4657c9538ed9168820
VBuser <timur.lihgt@rambler.ru>
2024-07-15 07:13:16 +0900

    Добавл решение Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+// Пример:
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+void Main()
+{
+    int[] array = { 1, 2, 5, 0, 10, 34 };
+    Print(array);
+    System.Console.WriteLine();
+}
+
+void Print(int[] array)
+{
+    System.Console.Write("[" + string.Join(", ",array) + "] => " );
+    PrintArrayRevers(array, array.Length - 1);
+}
+
+
+void PrintArrayRevers(int[] array, int i)
+{
+    if (i < 0) return;
+    System.Console.Write(array[i] + " ");
+    PrintArrayRevers(array, i - 1);
+}
+
+
+Main();
